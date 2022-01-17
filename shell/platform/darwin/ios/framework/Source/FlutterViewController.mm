9b9d88ace196835b1e91cf174d0ebfb074075443
Andrei Kulbeda <andrei_kulbeda@epam.com>
2022-01-17 20:24:49 +0300

    [Cherry Pick]fix support of blueetooth RCU

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -1583,9 +1583,9 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 // both places to capture keys both inside and outside of a text field, but have
 // slightly different implmentations.
 
+#if !(defined(TARGET_OS_TV) && TARGET_OS_TV)    
 - (void)pressesBegan:(NSSet<UIPress*>*)presses
-           withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {     
-  #if !(defined(TARGET_OS_TV) && TARGET_OS_TV)              
+           withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                 
   if (@available(iOS 13.4, tvOS 13.4, *)) {
     for (UIPress* press in presses) {
       [self handlePressEvent:[[[FlutterUIPressProxy alloc] initWithPress:press
@@ -1597,12 +1597,10 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
   } else {
     [super pressesBegan:presses withEvent:event];
   }
-  #endif
 }
 
 - (void)pressesChanged:(NSSet<UIPress*>*)presses
-             withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {
-  #if !(defined(TARGET_OS_TV) && TARGET_OS_TV)                         
+             withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                       
   if (@available(iOS 13.4, tvOS 13.4, *)) {
     for (UIPress* press in presses) {
       [self handlePressEvent:[[[FlutterUIPressProxy alloc] initWithPress:press
@@ -1614,12 +1612,10 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
   } else {
     [super pressesChanged:presses withEvent:event];
   }
-  #endif  
 }
 
 - (void)pressesEnded:(NSSet<UIPress*>*)presses
-           withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {
-  #if !(defined(TARGET_OS_TV) && TARGET_OS_TV)                                    
+           withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                             
   if (@available(iOS 13.4, tvOS 13.4, *)) {
     for (UIPress* press in presses) {
       [self handlePressEvent:[[[FlutterUIPressProxy alloc] initWithPress:press
@@ -1630,13 +1626,11 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
     }
   } else {
     [super pressesEnded:presses withEvent:event];
-  }
-  #endif   
+  } 
 }
 
 - (void)pressesCancelled:(NSSet<UIPress*>*)presses
-               withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {
-  #if !(defined(TARGET_OS_TV) && TARGET_OS_TV)                                        
+               withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                                    
   if (@available(iOS 13.4, tvOS 13.4, *)) {
     for (UIPress* press in presses) {
       [self handlePressEvent:[[[FlutterUIPressProxy alloc] initWithPress:press
@@ -1648,8 +1642,30 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
   } else {
     [super pressesCancelled:presses withEvent:event];
   }
-  #endif
 }
+
+#else
+
+- (void)pressesBegan:(NSSet<UIPress*>*)presses
+           withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                
+    [super pressesBegan:presses withEvent:event];
+}
+
+- (void)pressesChanged:(NSSet<UIPress*>*)presses
+             withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                       
+    [super pressesChanged:presses withEvent:event];
+}
+
+- (void)pressesEnded:(NSSet<UIPress*>*)presses
+           withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                                    
+    [super pressesEnded:presses withEvent:event];
+}
+
+- (void)pressesCancelled:(NSSet<UIPress*>*)presses
+               withEvent:(UIPressesEvent*)event API_AVAILABLE(ios(9.0),tvos(10.0)) {                                      
+    [super pressesCancelled:presses withEvent:event];
+}
+#endif
 
 #pragma mark - Orientation updates
 
