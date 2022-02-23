667abf1db05e595181d361793e6184a0380afce7
Andrei Kulbeda <andrei_kulbeda@epam.com>
2022-02-23 21:11:49 +0300

    fixed a UIScribbleInteraction

--- shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
@@ -2571,6 +2571,7 @@ return false;
 #pragma mark - Methods related to Scribble support
 
 - (void)setupIndirectScribbleInteraction:(id<FlutterViewResponder>)viewResponder {
+#if !(defined(TARGET_OS_TV) && TARGET_OS_TV)
   if (_viewResponder != viewResponder) {
     if (@available(iOS 14.0, *)) {
       UIView* parentView = viewResponder.view;
@@ -2582,6 +2583,7 @@ return false;
     }
   }
   _viewResponder = viewResponder;
+#endif
 }
 
 - (void)resetViewResponder {
