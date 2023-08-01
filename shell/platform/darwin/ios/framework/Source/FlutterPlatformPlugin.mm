b523f79428714633ef3c80e6339652e1ba984cf9
Aleksandr Denisov <denisov.shureg@gmail.com>
2023-08-01 12:49:51 +0200

    update to 3.10.5

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformPlugin.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformPlugin.mm
@@ -299,9 +299,8 @@ using namespace flutter;
   #if !(defined(TARGET_OS_TV) && TARGET_OS_TV)
   return @{@"value" : @([UIPasteboard generalPasteboard].hasStrings)};
   #else
-  return @{@"value" : @(hasStrings)};
+  return @{@"value" : @false};
   #endif
-
 }
 
 - (BOOL)isLiveTextInputAvailable {
