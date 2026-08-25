928bde177f83a1c937ecbe08b347a91a564c9621
Jürgen Wölke <juergen.woelke@infosys.com>
2026-08-25 16:28:38 +0200

    Update ninja_build.sh
    Fix for "snapshot_utils.cc: 137: error: Failed to memory map snapshot:" error

--- engine/src/ninja_build.sh
+++ engine/src/ninja_build.sh
@@ -27,19 +27,17 @@ if [[ "$1" == "clean" ]]; then
     rm -rf ./out/host_release$OUTPUT_POSTFIX
 fi
 if [[ "$1" == "clean" ]] || [[ ! -d ./out/host_release$OUTPUT_POSTFIX ]]; then
-   ./flutter/tools/gn --no-goma --no-lto --runtime-mode=release --no-enable-unittests --mac-cpu=$HOST_CPU
+   ./flutter/tools/gn --no-goma --no-lto --runtime-mode=release --no-enable-unittests --mac-cpu=$HOST_CPU --no-prebuilt-dart-sdk
 fi
 ninja -C out/host_release$OUTPUT_POSTFIX -j 4
-
 if [[ "$1" == "clean" ]] || [[ ! -d ./out/host_debug_unopt$OUTPUT_POSTFIX ]]; then
-   ./flutter/tools/gn --no-goma --unoptimized --no-enable-unittests --mac-cpu=$HOST_CPU
+   ./flutter/tools/gn --no-goma --unoptimized --no-enable-unittests --mac-cpu=$HOST_CPU --no-prebuilt-dart-sdk
 fi
 ninja -C out/host_debug_unopt$OUTPUT_POSTFIX -j 4
 if [[ "$1" == "clean" ]] || [[ ! -d ./out/ios_debug_sim_unopt$OUTPUT_POSTFIX ]]; then
-    ./flutter/tools/gn --ios --no-goma --simulator --unoptimized --simulator-cpu=$SIM_CPU #--no-prebuilt-dart-sdk
+    ./flutter/tools/gn --ios --no-goma --simulator --unoptimized --simulator-cpu=$SIM_CPU --no-prebuilt-dart-sdk
 fi
 ninja -C out/ios_debug_sim_unopt$OUTPUT_POSTFIX
-
 if [[ "$1" == "clean" ]] || [[ ! -d ./out/ios_debug_unopt$OUTPUT_POSTFIX ]]; then
     ./flutter/tools/gn --ios --no-goma --unoptimized --mac-cpu=$HOST_CPU --no-prebuilt-dart-sdk
 fi
